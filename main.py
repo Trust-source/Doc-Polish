f07989caa64c186fb32cf3f5d65b9a9ba5a50be0
tolorunf <ftrust001@gmail.com>
2026-09-23 08:50:06 -0500

    started the classify_role function

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ def extract_paragraphs(docx_path):
     paragraphs = []
 
     for para in doc.paragraphs:
+
+        if para.runs:
+            first_run = para.runs[0]
+            bold = first_run.bold
+            if first_run.font.size:
+                font_size = first_run.font.size.pt
         paragraphs.append({
             "text": para.text,
-            "style": para.style.name
+            "style": para.style.name,
+            "bold": bold,
+            "font_size": font_size
         })
 
     return paragraphs
